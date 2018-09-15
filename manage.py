c26709d027007a74903f69598b796fce2cc47d37
Evohmike <evohmik@gmail.com>
2018-09-15 12:17:10 +0300

    created manage.py

---
+++ manage.py
@@ -0,0 +1,30 @@
+from app import create_app
+from flask_script import Manager, Server
+
+
+# Creating app instance
+app = create_app('development')
+
+# create manager instance
+manager = Manager(app)
+
+manager.add_command('server', Server)
+
+
+if __name__ == '__main__':
+    manager.run()
+from app import create_app
+from flask_script import Manager, Server
+
+
+# Creating app instance
+app = create_app('development')
+
+# create manager instance
+manager = Manager(app)
+
+manager.add_command('server', Server)
+
+
+if __name__ == '__main__':
+    manager.run()
