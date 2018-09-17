ba278bbd2f2488ba2312376ba079e1586cecee03
Evohmike <evohmik@gmail.com>
2018-09-17 12:16:52 +0300

    initial migration

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,abort
 from . import main
 from flask_login import login_required,current_user
+from ..models import User
 #from .forms import CategoryForm
 
 # Views
@@ -12,5 +13,21 @@ def index():
     '''
 
 
-    title = "Welcome | One Minute Pitch"
+    title = "Welcome | Personal-Blog"
     return render_template('index.html',title=title)
+
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        about(404)
+
+    return render_template("profile/profile.html", user = user)
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -32,3 +32,17 @@ class User(db.Model,UserMixin):
         return f'User {self.username}'
 
 
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+    
+
+
+
+
+
